106f964813d0cc21431c8b9a66004d2eeb6ecc70
Felix Leung <>
2019-11-12 18:46:22 +0800

    add no cors

--- nodejs/index.js
+++ nodejs/index.js
@@ -18,4 +18,24 @@ var db = mongoose.connection;
 db.on("error", error => console.error(error));
 db.once("open", () => console.log("connected to database"));
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.listen(port);
